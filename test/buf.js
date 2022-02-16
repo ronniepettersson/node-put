d88d3b7bf5d2cd50d4d3fc7983bcb848648199c7
Ronnie Pettersson <rope011@outlook.com>
2022-02-16 07:24:01 -0800

    Changed to mocha test framework

--- test/buf.js
+++ test/buf.js
@@ -1,76 +1,113 @@
-import assert from 'assert';
-import Put from '../index.js';
-import Binary  from 'binary';
+import assert from "assert";
+import Put from "../index.js";
+import Binary from "binary";
 
-const chain = () => {
-    var stringBuffer = Buffer.from("pow");
-    var buf = Put()
-        .word16be(1337)
-        .word8(1)
-        .pad(5)
-        .put(stringBuffer)
-        .word32le(9000)
-        .word64le(3)
-        .word64be(4)
-        .buffer()
-    ;
-    assert.equal(buf.length, 2 + 1 + 5 + 3 + 4 + 8 + 8);
-    var bs = [].slice.call(buf);
-    
-    // word16be(1337)
-    assert.deepEqual(bs.slice(0,2), [ 0x05, 0x39 ]);
-    
-    // word8(1)
-    assert.deepEqual(bs.slice(2,3), [ 0x01 ]);
-    
-    // pad(5)
-    assert.deepEqual(bs.slice(3,8), [ 0x00, 0x00, 0x00, 0x00, 0x00 ]);
-    
-    // put(new Buffer('pow', 'ascii'))
-    assert.deepEqual(bs.slice(8,11), [ 0x70, 0x6f, 0x77 ]);
-    
-    // word32le(9000)
-    assert.deepEqual(bs.slice(11,15), [ 0x28, 0x23, 0x00, 0x00 ]);
-    
-    // word64le(3)
-    assert.deepEqual(
-        bs.slice(15,23),
-        [ 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 ]
-    );
-    
-    // word64be(4)
-    assert.deepEqual(
-        bs.slice(23,31),
-        [ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04 ]
-    );
-};
+describe("String buffer combination test", function () {
+  var stringBuffer = Buffer.from("pow");
+  var buf = Put()
+    .word16be(1337)
+    .word8(1)
+    .pad(5)
+    .put(stringBuffer)
+    .word32le(9000)
+    .word64le(3)
+    .word64be(4)
+    .buffer();
 
-const parity = () => {
-    [ 'le', 'be' ].forEach(function (end) {
-        [ 8, 16, 32 ].forEach(function (n) {
-            var max = Math.pow(2,n);
-            var step = Math.max(1, Math.floor(max / 1000));
-            
-            for (var i = 0; i < max; i += step) {
-                var buf = Put()[ 'word' + n + end ](i).buffer();
-                var j = Binary.parse(buf)[ 'word' + n + end ]('j').vars.j;
-                assert.deepEqual(i, j);
-            }
-        });
+  describe("Buffer length", function () {
+    it("buffer should be " + (2 + 1 + 5 + 3 + 4 + 8 + 8), function () {
+      assert.equal(buf.length, 2 + 1 + 5 + 3 + 4 + 8 + 8);
     });
-};
+  });
 
-const b64 = () => {
+  var bs = [].slice.call(buf);
+
+  describe("word16be", function () {
+    it("should return false when not equal", function () {
+      // word16be(1337)
+      assert.deepEqual(bs.slice(0, 2), [0x05, 0x39]);
+    });
+  });
+
+  describe("word8", function () {
+    it("should return false when not equal", function () {
+      // word8(1)
+      assert.deepEqual(bs.slice(2, 3), [0x01]);
+    });
+  });
+
+  describe("pad", function () {
+    it("should return false when not equal", function () {
+      // pad(5)
+      assert.deepEqual(bs.slice(3, 8), [0x00, 0x00, 0x00, 0x00, 0x00]);
+    });
+  });
+
+  describe("put(new Buffer('pow', 'ascii'))", function () {
+    it("should return false when not equal", function () {
+      // put(new Buffer('pow', 'ascii'))
+      assert.deepEqual(bs.slice(8, 11), [0x70, 0x6f, 0x77]);
+    });
+  });
+
+  describe("word32le", function () {
+    it("should return false when not equal", function () {
+      // word32le(9000)
+      assert.deepEqual(bs.slice(11, 15), [0x28, 0x23, 0x00, 0x00]);
+    });
+  });
+
+  describe("word64le", function () {
+    it("should return false when not equal", function () {
+      // word64le(3)
+      assert.deepEqual(
+        bs.slice(15, 23),
+        [0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+      );
+    });
+  });
+
+  describe("word16be", function () {
+    it("should return false when not equal", function () {
+      // word64be(4)
+      assert.deepEqual(
+        bs.slice(23, 31),
+        [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04]
+      );
+    });
+  });
+});
+
+describe("word [8|16|32|64] [le|be]", function () {
+  ["le", "be"].forEach(function (end) {
+    [8, 16, 32].forEach(function (n) {
+      var max = Math.pow(2, n);
+      var step = Math.max(1, Math.floor(max / 1000));
+
+      describe("word" + n + end, function () {
+        it("should return false when not equal", function () {
+          for (var i = 0; i < max; i += step) {
+            var buf = Put()["word" + n + end](i).buffer();
+            var j = Binary.parse(buf)["word" + n + end]("j").vars.j;
+            assert.deepEqual(i, j);
+          }
+        });
+      });
+    });
+  });
+
+
+describe("word64be", function () {
+  it("should return false when not equal", function () {
     var buf = Put().word64be(1).buffer();
-    assert.deepEqual([].slice.call(buf), [ 0, 0, 0, 0, 0, 0, 0, 1 ]);
-};
+    assert.deepEqual([].slice.call(buf), [0, 0, 0, 0, 0, 0, 0, 1]);
+  });
+});
 
-const l64 = () => {
+describe("word64le", function () {
+  it("should return false when not equal", function () {
     var buf = Put().word64le(1).buffer();
-    assert.deepEqual([].slice.call(buf), [ 1, 0, 0, 0, 0, 0, 0, 0 ]);
-};
-
-chain();
-parity();
-b64();
-l64();
+    assert.deepEqual([].slice.call(buf), [1, 0, 0, 0, 0, 0, 0, 0]);
+  });
+});
+});
--- test/stream.js
+++ test/stream.js
@@ -1,31 +1,32 @@
 import assert from "assert";
-import Put from '../index.js';
+import Put from "../index.js";
 
 //exports.stream = function () {
-const stream = () => {    
+
+describe("stream write", function () {
+  
     var written = 0;
     var stream = {
-        write : function (buf) {
-            assert.equal(buf.toString(), 'abcdefg');
-            written ++;
-        }
+      write: function (buf) {
+        it("should return false when not equal", function () {   
+            assert.equal(buf.toString(), "abcdefg");
+        });
+        written++;
+      },
     };
-    
-    Put()
-        .word16be(
-            256 * 'a'.charCodeAt(0)
-            + 'b'.charCodeAt(0)
-        )
-        .word32le(
-            Math.pow(256, 0) * 'c'.charCodeAt(0)
-            + Math.pow(256, 1) * 'd'.charCodeAt(0)
-            + Math.pow(256, 2) * 'e'.charCodeAt(0)
-            + Math.pow(256, 3) * 'f'.charCodeAt(0)
-        )
-        .word8('g'.charCodeAt(0))
-        .write(stream)
-    ;
-    assert.equal(written, 1);
-};
 
-stream();
+    Put()
+      .word16be(256 * "a".charCodeAt(0) + "b".charCodeAt(0))
+      .word32le(
+        Math.pow(256, 0) * "c".charCodeAt(0) +
+          Math.pow(256, 1) * "d".charCodeAt(0) +
+          Math.pow(256, 2) * "e".charCodeAt(0) +
+          Math.pow(256, 3) * "f".charCodeAt(0)
+      )
+      .word8("g".charCodeAt(0))
+      .write(stream);
+
+    it("should return false when not equal", function () {  
+        assert.equal(written, 1);
+    });    
+});
